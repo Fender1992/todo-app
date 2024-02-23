bc6343e8806553c81276152d680dfd6693780083
Rolando Fender <92167269+Fender1992@users.noreply.github.com>
2024-02-23 10:03:48 -0600

    deploy

--- src/app/Header/header/header.component.ts
+++ src/app/Header/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Item } from 'src/app/Model/items.model';
-import { CalenderService } from 'src/app/Services/calender.service';
 import { DateService } from 'src/app/Services/date.service';
 import { HandleItems } from 'src/app/Services/handleItems.service';
 
@@ -15,7 +13,6 @@ export class HeaderComponent implements OnInit {
   currentDate: any;
   @Output() taskAdded = new EventEmitter<Item>();
   taskInput: string = '';
-  @Input('previosDays') daysArray: CalenderService[] = [];
   constructor(
     private dateService: DateService,
     private handleItems: HandleItems
