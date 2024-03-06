fbcb9535b01a4df5a26890691b84e5dd60060f66
Rolando Fender <92167269+Fender1992@users.noreply.github.com>
2024-03-06 15:22:28 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-import { HandleTodoItemComponent } from './handle-todo-item/handle-todo-item.component';
 import { MainComponent } from './main/main.component';
 import { FormsModule } from '@angular/forms';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
@@ -28,7 +27,6 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     TodoListComponent,
-    HandleTodoItemComponent,
     SignInComponent,
     MainComponent,
     LoadingSpinner,
