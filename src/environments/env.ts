f733ba2c9f158af6d582f23a0866de9b831dd419
Rolando Fender <92167269+Fender1992@users.noreply.github.com>
2024-04-08 10:49:07 -0400

    env generation

---
+++ src/environments/env.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCl2E-Qmu4fOYNl_ZppPdgB7PECzwzpC6w',
+  authDomain: 'todo-app-30b79.firebaseapp.com',
+  databaseURL: 'https://todo-app-30b79-default-rtdb.firebaseio.com',
+  projectId: 'todo-app-30b79',
+  storageBucket: 'todo-app-30b79.appspot.com',
+  messagingSenderId: '903444535483',
+  appId: '1:903444535483:web:169503236c5a96adb8719e',
+  measurementId: 'G-H21PZ4DEEE',
+};
